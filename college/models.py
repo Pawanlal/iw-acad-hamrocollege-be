18b756c5419a934487e48b0e992d9542a7b06e0e
pawanlal <pawanlalganesh@gmail.com>
2020-08-14 19:32:15 +0545

    Updated college model

--- college/models.py
+++ college/models.py
@@ -1,35 +1,25 @@
-#from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.db import models
 
 
-class Faculty(models.Model):    
+class Faculty(models.Model):
     faculty_name = models.CharField( max_length=70)
-           
+
     def __str__(self):
         return self.faculty_name
 
-class SemesterList(models.Model):
-	SEMESTER_CODE = (('i','i'), ('ii', 'ii'), ('iii','iii'), ('iv','iv'), ('v','v'), ('vi','vi'), ('vii','vii'), ('viii','viii'))
-	code = models.CharField(choices=SEMESTER_CODE, max_length=4, primary_key=True)
-	name = models.CharField(max_length=50)
 
-	def __str__(self):
-		return f'{self.code}'
+class SemesterList(models.Model):
+    semester = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.semester
 
 
 class SubjectList(models.Model):
-    code=models.CharField(max_length=70)
-    name=models.CharField(max_length=255)
-    target_faculty=models.ManyToManyField(Faculty, related_name='subject_list')
-    target_semester=models.ManyToManyField(SemesterList, related_name='subject_list')
-
+    subject_code=models.CharField(max_length=30)
+    name = models.CharField(max_length=255)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    semester = models.ForeignKey(SemesterList, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-
-
-
-    
-
-
-
