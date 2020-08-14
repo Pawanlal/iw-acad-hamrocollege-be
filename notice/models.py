3d3b2679578658f0d03384e8bf44ad29a4a211e1
pawanlal <pawanlalganesh@gmail.com>
2020-08-14 16:59:54 +0545

    Created notice app, notice model and category model

---
+++ notice/models.py
@@ -0,0 +1,53 @@
+import uuid
+
+from django.conf import settings
+from django.db import models
+from django.db.models.signals import pre_save, post_delete
+from django.dispatch import receiver
+from django.utils.text import slugify
+
+
+def file_location(instance, filename):
+    extension = filename.split('.')[-1]
+    author_id = str(instance.author.id)
+    unique_id = str(uuid.uuid4())
+    new_filename = unique_id+'.'+extension
+
+    file_path = 'collegenotice/{author_id}/{new_filename}'.format(
+        author_id=author_id,
+        new_filename=new_filename
+    )
+    return file_path
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category
+
+
+class Notice(models.Model):
+    text = models.TextField(max_length=5000)
+    file = models.FileField(upload_to=file_location, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_published = models.DateTimeField(auto_now_add=True, verbose_name="date published")
+    date_updated = models.DateTimeField(auto_now=True, verbose_name="date updated")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return self.slug
+
+
+@receiver(post_delete, sender=Notice)
+def submission_delete(sender, instance, **kwargs):
+    instance.file.delete(False)
+
+
+def pre_save_notice_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(uuid.uuid4())
+
+
+pre_save.connect(pre_save_notice_receiver, sender=Notice)
