2be307e76acbd08bbd43ae19b5c313efa409a24e
sujan <sujankhyaju2014@gmail.com>
2020-08-16 16:55:23 +0545

    Created Club app

---
+++ club/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+from account.models import User
+
+# Create your models here.
+
+class Club(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Member(models.Model):
+    id = models.AutoField(primary_key = True)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_creator = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.club.name + '-' + self.user.name
+
+    class Meta:
+        ordering = ['club']
+
+
+class Announcement(models.Model):
+    id = models.AutoField(primary_key=True)
+    club = models.ForeignKey(Club, on_delete= models.CASCADE)
+    announce_by = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    modified_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.club.name + '-' + self.message
+
+
